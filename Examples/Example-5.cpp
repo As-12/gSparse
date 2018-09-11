e02e949a9f499e1c6d81987e484739cb6db439c6
As-12 <as12production@gmail.com>
2018-09-11 23:31:26 +0100

    Add visualization

--- Examples/Example-5.cpp
+++ Examples/Example-5.cpp
@@ -16,18 +16,35 @@
 int main()
 {
     
+   /*gSparse::Builder::createRandomGraph(2,1,30,"dumbell-30.csv");
+    gSparse::Builder::createRandomGraph(3,4,30,"3-Community-30.csv");
+    gSparse::Builder::createRandomGraph(4,8,30,"4-Community-30.csv");
+    gSparse::Builder::createRandomGraph(3,8,1000,"3-Community-1000.csv");
+    gSparse::GraphCSVWriter writer("Complete-1000.csv","None"," ");
+    gSparse::GraphCSVWriter writer2("Complete-30.csv","None"," ");
+    gSparse::GraphCSVWriter writer3("Complete-2000.csv","None"," ");
+
+    writer.Write(gSparse::Builder::buildUnitCompleteGraph(1000)->GetEdgeList());
+    writer3.Write(gSparse::Builder::buildUnitCompleteGraph(2000)->GetEdgeList());
+    writer2.Write(gSparse::Builder::buildUnitCompleteGraph(30)->GetEdgeList());*/
+
+    //writer.Write(std::make_shared<gSparse::UndirectedGraph>(gSparse::Builder::buildUnitCompleteGraph(30)));
+    //writer.Write(std::make_shared<gSparse::UndirectedGraph>(gSparse::Builder::buildUnitCompleteGraph(2000)));
     gSparse::GraphReader reader = std::make_shared<gSparse::GraphCSVReader>("../Dataset/dumbell-30.csv","None"," ");
     gSparse::Graph graph = std::make_shared<gSparse::UndirectedGraph>(reader);
     gSparse::SpectralSparsifier::ERSampling sparsifier(graph);
 
     sparsifier.SetC(100);
     sparsifier.SetEpsilon(0.75);
-    
+    std::cout<<"ER"<<std::endl;
     sparsifier.Compute();
+    std::cout<<"Sparsifying"<<std::endl;
     auto result = sparsifier.GetSparsifiedGraph();
 
-    gSparse::GraphCSVWriter writer("sparsifier-dumbell-30-eps75.csv");
+    gSparse::GraphCSVWriter writer("sparsifier-dumbell-eps75-edges.csv",
+                                   "sparsifier-dumbell-eps75-weights.csv");
     writer.Write(result);
+    return EXIT_SUCCESS;
     return EXIT_SUCCESS;
 }
 
